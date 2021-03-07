03fb314eda81b81a173f9b0f1c4b13ec8f9820bc
Michal <michal.pidanic1@gmail.com>
2021-03-07 16:29:55 +0100

    production setup in settings.py

--- dbs_project/settings.py
+++ dbs_project/settings.py
@@ -17,17 +17,17 @@ import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-# getting sensitive data such as passwords from secrets.json
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
+# # getting sensitive data such as passwords from secrets.json
+# with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
+#     secrets = json.load(secrets_file)
 
 
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
+# def get_secret(setting, secrets=secrets):
+#     """Get secret setting or fail with ImproperlyConfigured"""
+#     try:
+#         return secrets[setting]
+#     except KeyError:
+#         raise ImproperlyConfigured("Set the {} setting".format(setting))
 
 
 # Quick-start development settings - unsuitable for production
@@ -88,24 +88,24 @@ WSGI_APPLICATION = 'dbs_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # # production
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': os.environ.get('DBNAME'),
-    #     'HOST': 'fiit-dbs-xpidanic-db.postgres.database.azure.com',
-    #     'USER': 'postgres@fiit-dbs-xpidanic-db',
-    #     'PASSWORD': os.environ.get('DBPASS'),
-    #     'PORT': '5432'
-    # }
-    # localhost
+    # production
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_secret('DB_NAME'),
-        'USER': get_secret('DB_USER'),
-        'PASSWORD': get_secret('DB_PASSWORD'),
-        'HOST': get_secret('DB_HOST'),
+        'NAME': os.environ.get('DBNAME'),
+        'HOST': 'fiit-dbs-xpidanic-db.postgres.database.azure.com',
+        'USER': 'postgres@fiit-dbs-xpidanic-db',
+        'PASSWORD': os.environ.get('DBPASS'),
         'PORT': '5432'
     }
+    # localhost
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': get_secret('DB_NAME'),
+    #     'USER': get_secret('DB_USER'),
+    #     'PASSWORD': get_secret('DB_PASSWORD'),
+    #     'HOST': get_secret('DB_HOST'),
+    #     'PORT': '5432'
+    # }
 }
 
 
